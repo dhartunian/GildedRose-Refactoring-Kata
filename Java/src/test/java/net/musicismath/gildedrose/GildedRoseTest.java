53e2babd3d9a02a3442a867724219449f90bd9b7
David Hartunian <davidh@knewton.com>
2015-11-25 16:41:27 -0500

    A test passes

--- Java/src/test/java/net/musicismath/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/net/musicismath/gildedrose/GildedRoseTest.java
@@ -2,7 +2,7 @@ package net.musicismath.gildedrose;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GildedRoseTest {
 
@@ -11,7 +11,16 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(app.items[0].name).isEqualTo("foo");
+    }
+
+    @Test
+    public void updateQuality_LowersSellInAndQuality() {
+        Item item = new Item("foo", 10, 20);
+        GildedRose app = new GildedRose(new Item[] {item});
+        app.updateQuality();
+        assertThat(app.items[0].sellIn).isEqualTo(9);
+        assertThat(app.items[0].quality).isEqualTo(19);
     }
 
 }
