224e72dd7e608bcf58ff76f9ff4f1e87d1128c84
David Hartunian <davidh@knewton.com>
2015-11-25 16:46:41 -0500

    Update tests to use only Item

--- Java/src/test/java/net/musicismath/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/net/musicismath/gildedrose/GildedRoseTest.java
@@ -8,19 +8,16 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app.items[0].name).isEqualTo("foo");
+        Item item = new Item("foo", 0, 0);
+        assertThat(item.name).isEqualTo("foo");
     }
 
     @Test
     public void updateQuality_LowersSellInAndQuality() {
         Item item = new Item("foo", 10, 20);
-        GildedRose app = new GildedRose(new Item[] {item});
-        app.updateQuality();
-        assertThat(app.items[0].sellIn).isEqualTo(9);
-        assertThat(app.items[0].quality).isEqualTo(19);
+        item.updateQuality();
+        assertThat(item.sellIn).isEqualTo(9);
+        assertThat(item.quality).isEqualTo(19);
     }
 
 }
