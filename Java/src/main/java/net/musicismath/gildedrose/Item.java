650a0b823c22646b15d266234067fdd4afdd6cc6
David Hartunian <davidh@knewton.com>
2015-11-25 16:42:56 -0500

    Add lombok

--- Java/src/main/java/net/musicismath/gildedrose/Item.java
+++ Java/src/main/java/net/musicismath/gildedrose/Item.java
@@ -1,21 +1,14 @@
 package net.musicismath.gildedrose;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Item {
 
     public String name;
-
     public int sellIn;
-
     public int quality;
 
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
-
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
 }
