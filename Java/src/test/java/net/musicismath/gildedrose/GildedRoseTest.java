d9b88f3e9b2782cb51a83378d27117831a1a4f74
David Hartunian <davidh@knewton.com>
2015-11-28 16:07:13 -0500

    Two new tests for specs

--- Java/src/test/java/net/musicismath/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/net/musicismath/gildedrose/GildedRoseTest.java
@@ -17,10 +17,48 @@ public class GildedRoseTest {
     @Test
     public void updateQuality_LowersSellInAndQuality() {
         Item item = new Item("foo", 10, 20);
-        GildedRose app = new GildedRose(new Item[] {item});
+        GildedRose app = wrapInApp(item);
         app.updateQuality();
         assertThat(app.items[0].sellIn).isEqualTo(9);
         assertThat(app.items[0].quality).isEqualTo(19);
+    }
+
+    @Test
+    public void updateQuality_afterSellInQualityDegradesTwiceAsFast() {
+        Item item = new Item("foo", 2, 20);
+        GildedRose app = wrapInApp(item);
+
+        app.updateQuality();
+        assertThat(getSingleItem(app).quality).isEqualTo(19);
+        app.updateQuality();
+        assertThat(getSingleItem(app).quality).isEqualTo(18);
+        app.updateQuality();
+        assertThat(getSingleItem(app).quality).isEqualTo(16);
+        app.updateQuality();
+        assertThat(getSingleItem(app).quality).isEqualTo(14);
+        app.updateQuality();
+        assertThat(getSingleItem(app).quality).isEqualTo(12);
+    }
+
+    @Test
+    public void updateQuality_neverNegative() {
+        Item item = new Item("foo", 2, 1);
+        GildedRose app = wrapInApp(item);
+
+        app.updateQuality();
+        assertThat(getSingleItem(app).quality).isEqualTo(0);
+        app.updateQuality();
+        assertThat(getSingleItem(app).quality).isEqualTo(0);
+        app.updateQuality();
+        assertThat(getSingleItem(app).quality).isEqualTo(0);
+    }
+
+    private Item getSingleItem(GildedRose app) {
+        return app.items[0];
+    }
+
+    private GildedRose wrapInApp(Item item) {
+        return new GildedRose(new Item[] {item});
     }
 
 }
